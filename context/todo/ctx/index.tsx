ed899aa2174aff885eafcf834881edb0de5b68e7
Hiro-mackay <johnmackay150@gmail.com>
2020-10-12 22:25:01 +0900

    add todo context

---
+++ context/todo/ctx/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createCtx } from "@/utiles/createCtx";
+import { Todo } from "../entity";
+import { ContextProviderType } from "@/utiles/contextProviderType";
+import { useTodo } from "@/hooks/useTodo";
+
+export interface TodoContextType extends ContextProviderType {
+  todo: Todo;
+  add: (todo:Todo) => null;
+  delete: (todoId: Todo['id']) => null;
+  update: (todoId: Todo['id'], title: Todo['title']) => null;
+};
+
+const [useTodoContext, TodoContext] = createCtx<TodoContextType>();
+
+const TodoProvider: React.FC = ({ children }) => {
+  const todo = useTodo();
+  return <TodoContext.Provider value={todo}>{children}</TodoContext.Provider>
+};
+
+export { useTodoContext, TodoContext, TodoProvider };
