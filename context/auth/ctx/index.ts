1d56c1e1da400c621c0333aeed4c5f7e500a5a06
Hiro-mackay <johnmackay150@gmail.com>
2020-10-12 21:26:53 +0900

    add timer entity type

--- context/auth/ctx/index.ts
+++ context/auth/ctx/index.ts
@@ -1,17 +1,16 @@
-import { createCtx } from '@/utiles/createCtx';
-import { Credintial } from '@/authCtx/entityType';
-import {ContextProviderType} from '@/utiles/contextProviderType'
-
+import { createCtx } from "@/utiles/createCtx";
+import { Credintial } from "@/authCtx/entityType";
+import { ContextProviderType } from "@/utiles/contextProviderType";
 
 interface AuthProviderType extends ContextProviderType {
   credintial: Credintial;
   setCredential: any;
   logIn: any;
   signUp: any;
-  logOut: any
+  logOut: any;
 }
 
 const [useAuthContext, authContext] = createCtx<AuthProviderType>();
-const authProvider = authContext.Provider
+const authProvider = authContext.Provider;
 
-export { useAuthContext , authContext, authProvider};
+export { useAuthContext, authContext, authProvider };
--- context/timer/entityType/index.ts
+++ context/timer/entityType/index.ts
@@ -1,3 +1,11 @@
 export interface Timer {
-  
+  id: string;
+  timerTitle: string;
+  isWorking: boolean;
+  createdBy: Date;
+  startedDate: Date;
+  totalMilliSeconds: number;
+  ownerId: string;
+  currentTaskId: string;
+  timerType: "personal" | "group";
 }
